8ed211917ff89af23eb5915f7ddbcfdb1c6ac277
Nicolas de Figueiredo <nicolasfigueiredo381@gmail.com>
2023-12-15 13:04:55 -0300

    Página do 451 criada. Página de empréstimo geral deletada.

--- cadastro.js
+++ cadastro.js
@@ -2,12 +2,12 @@
 var cadastro = document.getElementById('cad');
 
 // Define o atributo 'action'
-cadastro.action = '127.0.0.1:5500/confirmacao.html';
+cadastro.action = "127.0.0.1:5500/confirmacao.html";
 
 // Define o atributo 'method'
-cadastro.method = 'POST';
+cadastro.method = "post";
 
-if(cadastro != ""){
-    alert(cadastro)
-}
+const usuario = document.querySelector('#usuario');
 
+const outro = usuario.value;
+console.log(outro);
